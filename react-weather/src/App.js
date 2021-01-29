6583095ca793e9a7be4273ea21db11eecb58bd58
Karan Chauhan <KChauhan@reena.org>
2021-01-29 10:35:10 -0500

    Created By Karan

---
+++ react-weather/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Tempapp from './Components/Tempapp'
+
+const App = ()=> {
+  return (
+    <>
+    <Tempapp/>   
+      
+    </>
+  );
+}
+
+export default App;
---
+++ react-weather/src/Components/WeatherIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const WeatherIcon = (props)=> {
+    return (
+        <>
+        <img className="weather_icon" src="images\clouds.png" alt="clouds"/>            
+        </>
+    )
+}
+
+export default WeatherIcon
